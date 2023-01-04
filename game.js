0cbbcbabe9ef7e6a45894fc6b383059e6f9ba77f
Lucas <62632191+LucasNLS@users.noreply.github.com>
2023-01-04 11:43:26 -0600

    Implemented method to draw random computer guess

---
+++ game.js
@@ -0,0 +1,10 @@
+console.log("hi");
+
+function getComputerChoice() {
+  let selection = Math.floor(Math.random() * 3);
+  console.log(selection);
+}
+
+function playRound(playerSelection, computerSelection) {}
+
+getComputerChoice();
